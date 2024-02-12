2c7b35281e4721186dca4525eb0313f619702914
Kugesz <81580775+Kugesz@users.noreply.github.com>
2024-02-12 13:06:21 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+function mineLocation(field) {
+  const cord = [];
+  for (let i = 0; i < field.length; i++) {
+    if (field[i].includes(1)) {
+      return [i,field[i].indexOf(1)];
+    }
+  } 
+}
